df11108bc9c9478ebf4666ea471ffcdee87b2df5
jo053066 <jao@oldchool.not.br>
2022-09-08 21:52:46 -0300

    d

--- PJ1/client.py
+++ PJ1/client.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     with socket(AF_INET, SOCK_DGRAM) as s:
         s.connect((sys.argv[1], int(sys.argv[2])))
         while(1):
-            value = input("Please enter a string:\n")
-            s.sendall(value.encode()+"\n")
+            value = input("Please enter a string:\n") + "\n"
+            s.sendall(value.encode())
             #data = s.recv(1024)
 
     print(f"Received {data!r}")
